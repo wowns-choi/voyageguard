09bfa65e7a9e367bafce5c857df7621fb68043fc
wowns-choi <154235704+wowns-choi@users.noreply.github.com>
2026-08-31 22:41:11 +0900

    chore: 로컬 개발 환경 세팅
    - Spring Boot 백엔드(voyageguard) 초기 설정
    - React + TS + Vite 프론트엔드(voyageguard-front) 스캐폴딩
    - docker-compose로 MySQL/Redis/Kafka 로컬 인프라 구성

---
+++ voyageguard/src/main/java/com/voyageguard/VoyageguardApplication.java
@@ -0,0 +1,13 @@
+package com.voyageguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyageguardApplication.class, args);
+	}
+
+}
---
+++ voyageguard/src/test/java/com/voyageguard/VoyageguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyageguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
